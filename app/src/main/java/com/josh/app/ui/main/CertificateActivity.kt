c3b8ddde04029e91db1d8f9e7654e9b8c032dece
hanif <haniefhelmi38@gmail.com>
2021-09-20 12:59:31 +0700

    First Commit

---
+++ app/src/main/java/com/josh/app/ui/main/CertificateActivity.kt
@@ -0,0 +1,11 @@
+package com.josh.app.ui.main
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CertificateActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_certificate)
+    }
+}
