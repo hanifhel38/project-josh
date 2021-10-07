f03e47c9666975c8e47e5f33c8e7963544f29628
muham <muhammadhassan7788@gmail.com>
2021-10-07 13:09:42 +0700

    Optimize Import

--- app/src/main/java/com/josh/app/ui/main/ActivitySilabus.kt
+++ app/src/main/java/com/josh/app/ui/main/ActivitySilabus.kt
@@ -2,6 +2,7 @@ package com.josh.app.ui.main
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.josh.app.R
 
 class ActivitySilabus : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
--- app/src/main/java/com/josh/app/ui/main/TestActivity.kt
+++ app/src/main/java/com/josh/app/ui/main/TestActivity.kt
@@ -2,6 +2,7 @@ package com.josh.app.ui.main
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.josh.app.R
 
 class TestActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
